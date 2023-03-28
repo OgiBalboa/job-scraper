39157ca02a7a53ec4a761e140786c4d0bbb54553
OgiBalboa <sismanogulcan@gmail.com>
2023-03-28 08:57:32 +0300

    Add filter crud functions.

--- services/job-scraper-api/src/main.py
+++ services/job-scraper-api/src/main.py
@@ -1,7 +1,9 @@
 import asyncio
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Path, Query
+from uuid import UUID
 
-from utils.jobs import get_jobs_by_platform, PlatformName, PLATFORM_URLS
+from utils.jobs import get_jobs_by_platform, PlatformName, PLATFORM_URLS, JobFilter
 
 app = FastAPI()
 
@@ -11,8 +13,14 @@ async def root():
     return {"message": "This is a job scraper api!"}
 
 
-@app.get("/get-jobs/")
-async def get_jobs(platform: PlatformName | None = None):
+@app.get("/get-jobs/{filter-id}")
+async def get_jobs(filter_id: int, platform: PlatformName | None = None):
+    """Get jobs from platform(s) by filter id.
+    :param filter_id: JobFilter model id. (platform_type of the filter must be universal
+        if you don't specify platform name.
+    :param platform: name of the platform.
+    :return: list of the jobs returned by API endpoint.
+    """
     if platform:
         return await get_jobs_by_platform(PLATFORM_URLS[platform], {})
     results = await asyncio.gather(
@@ -21,6 +29,31 @@ async def get_jobs(platform: PlatformName | None = None):
     return results
 
 
-@app.post("/job/filters")
-async def upsert_job_filter(job_filter: JobFilter):
-    pass
+@app.get("/job/filters/", response_model=list[JobFilter])
+async def read_job_filter(
+        id_: int = None,
+        name: Annotated[
+            str,
+            Query(
+                title='name',
+                description='query job filters by name',
+                min_length=1,
+                max_length=55
+            )
+        ] = None,
+        user_id: UUID = None
+):
+    return [{"id": id_, "name": name, "user_id": user_id}]
+
+
+@app.post("/job/filters/")
+async def create_job_filter(job_filter: JobFilter):
+    return job_filter
+
+
+@app.post("/job/filters/{job_filter_id}")
+async def update_job_filter(
+        job_filter_id: Annotated[int, Path(title='The ID of the item to get')],
+        job_filter: JobFilter
+):
+    return job_filter
--- services/job-scraper-api/src/utils/jobs.py
+++ services/job-scraper-api/src/utils/jobs.py
@@ -1,15 +1,23 @@
 from httpx import AsyncClient
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from uuid import UUID
 
 
 class JobFilter(BaseModel):
     name: str
-    search_text: str | None = None
+    search_text: str | None = Field(default=None, min_length=2)
     easy_apply: bool | None = None
+    locations: set[str] = set()
+    user_id: UUID
 
 
-async def get_jobs_by_platform(platform_name: str, url_params: dict, **kwargs):
+async def get_jobs_by_platform(
+        platform_name: str,
+        url_params: dict,
+        filter_id: int = 0,
+        **kwargs
+):
     url = PLATFORM_URLS[platform_name].format(url_params)
     async with AsyncClient() as client:
         await client.get(url, timeout=2 * 60, **kwargs)
